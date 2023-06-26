8f5380d3e87305b1794c4dd152940febe85c61ec
ninhtq97 <tranquangninh97@gmail.com>
2023-06-26 16:57:22 +0700

    Update typing pipe

--- dist/pipes/uploader.pipe.js
+++ dist/pipes/uploader.pipe.js
@@ -2,7 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.UploaderFileTypeValidator = void 0;
 const common_1 = require("@nestjs/common");
+const file_type_1 = require("file-type");
+const promises_1 = require("fs/promises");
+const path_1 = require("path");
 const uploader_constant_1 = require("../constants/uploader.constant");
+const uploader_util_1 = require("../utils/uploader.util");
 class UploaderFileTypeValidator extends common_1.FileValidator {
     constructor({ acceptMimeType = Object.values(uploader_constant_1.MIME_TYPE)
         .map((e) => e)
@@ -16,9 +20,24 @@ class UploaderFileTypeValidator extends common_1.FileValidator {
         if (!this.validationOptions) {
             return true;
         }
+        const parseFile = file;
+        const buffer = await (0, uploader_util_1.readChunk)(parseFile.path, { length: 4100 });
+        const { ext, mime } = await (0, file_type_1.fromBuffer)(buffer);
+        if (!this.validationOptions.acceptMimeType.includes(mime)) {
+            await (0, promises_1.unlink)(parseFile.path);
+            return false;
+        }
+        if (this.validationOptions.renameIfMimeWrong) {
+            console.log('=====================Intercept Rename File If Mime Wrong');
+            const name = (0, path_1.basename)(parseFile.filename, (0, path_1.extname)(parseFile.filename));
+            const filename = `${name}.${ext}`;
+            const path = `${parseFile.destination}/${filename}`;
+            await (0, promises_1.rename)(parseFile.path, path);
+            file = Object.assign(Object.assign({}, file), { mimetype: mime, filename, path: path });
+        }
         return (!!file &&
             'mimetype' in file &&
-            this.validationOptions.acceptMimeType.includes(file.mimetype));
+            this.validationOptions.acceptMimeType.includes(mime));
     }
 }
 exports.UploaderFileTypeValidator = UploaderFileTypeValidator;
--- lib/pipes/uploader.pipe.ts
+++ lib/pipes/uploader.pipe.ts
@@ -1,6 +1,10 @@
 import { FileValidator } from '@nestjs/common';
+import { fromBuffer } from 'file-type';
+import { rename, unlink } from 'fs/promises';
+import { basename, extname } from 'path';
 import { MIME_TYPE } from '../constants/uploader.constant';
 import { UploaderFileTypeValidatorOptions } from '../interfaces/uploader-validator-options';
+import { readChunk } from '../utils/uploader.util';
 
 interface IFile {
   mimetype: string;
@@ -28,28 +32,30 @@ export class UploaderFileTypeValidator extends FileValidator<UploaderFileTypeVal
       return true;
     }
 
-    // const buffer = await readChunk(file.path, { length: 4100 });
-    // const { ext, mime } = await fromBuffer(buffer);
+    const parseFile: Express.Multer.File = file as any;
 
-    // if (!this.validationOptions.acceptMimeType.includes(mime)) {
-    //   await unlink(file.path);
-    //   return false;
-    // }
+    const buffer = await readChunk(parseFile.path, { length: 4100 });
+    const { ext, mime } = await fromBuffer(buffer);
 
-    // if (this.validationOptions.renameIfMimeWrong) {
-    //   console.log('=====================Intercept Rename File If Mime Wrong');
-    //   const name = basename(file.filename, extname(file.filename));
-    //   const filename = `${name}.${ext}`;
-    //   const path = `${file.destination}/${filename}`;
+    if (!this.validationOptions.acceptMimeType.includes(mime)) {
+      await unlink(parseFile.path);
+      return false;
+    }
 
-    //   await rename(file.path, path);
-    //   file = { ...file, mimetype: mime, filename, path: path };
-    // }
+    if (this.validationOptions.renameIfMimeWrong) {
+      console.log('=====================Intercept Rename File If Mime Wrong');
+      const name = basename(parseFile.filename, extname(parseFile.filename));
+      const filename = `${name}.${ext}`;
+      const path = `${parseFile.destination}/${filename}`;
+
+      await rename(parseFile.path, path);
+      file = { ...file, mimetype: mime, filename, path: path };
+    }
 
     return (
       !!file &&
       'mimetype' in file &&
-      this.validationOptions.acceptMimeType.includes(file.mimetype)
+      this.validationOptions.acceptMimeType.includes(mime)
     );
   }
 }
