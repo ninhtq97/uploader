9fc70fd4deb19faae021bf3c1b1b4aa9fb7edd4c
ninhtq97 <tranquangninh97@gmail.com>
2023-08-03 11:39:22 +0700

    Add check require upload file

--- lib/interceptors/uploader-required.interceptor.ts
+++ lib/interceptors/uploader-required.interceptor.ts
@@ -4,18 +4,24 @@ import {
   ExecutionContext,
   Injectable,
   NestInterceptor,
+  Type,
+  mixin,
 } from '@nestjs/common';
 import { Request } from 'express';
 
-@Injectable()
-export class UploaderRequiredInterceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, next: CallHandler) {
-    const req = context.switchToHttp().getRequest<Request>();
+export function UploaderRequiredInterceptor(): Type<NestInterceptor> {
+  @Injectable()
+  class Interceptor implements NestInterceptor {
+    async intercept(context: ExecutionContext, next: CallHandler) {
+      const ctx = context.switchToHttp();
+      const req = ctx.getRequest<Request>();
 
-    if (!req.file && !req.files) {
-      throw new BadRequestException('File not found');
+      if (!req.file && !req.files) {
+        throw new BadRequestException('File not found');
+      }
+
+      return next.handle();
     }
-
-    return next.handle();
   }
+  return mixin(Interceptor);
 }
